067179d71c0515df1bb1efbb87eebc041bb4bc27
Nev <67838965+NevMoney@users.noreply.github.com>
2021-07-09 18:54:36 -0400

    removing not needed
    removing code we won't use
    removing file we don't need

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -2,26 +2,12 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-let sum = 0
-// type "fof" hit enter:
-for (let num of [1, 2, 3, 4, 5]) {
-  sum += num
-}
-
-// to make this, write "prom" hit enter:
-return (
-  new Promise((resolve, reject) => {})
-    // "thenc" hit enter:
-    .then((result) => {})
-    .catch((err) => {})
-)
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coin Exchange {sum}</h1>
+        <h1>Coin Exchange</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
