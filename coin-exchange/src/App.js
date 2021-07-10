9316337c0f8bc3aba5ff611126d8c7d26184f3df
Nev <67838965+NevMoney@users.noreply.github.com>
2021-07-09 21:12:55 -0400

    styling
    changed size of logo and h1
    changed the look of the table
    imported table into the app

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Coin from './components/Coin/Coin'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coin Exchange</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Coin Exchange</h1>
       </header>
+      <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price="$35,000" />
+          <Coin name="Ethereum" ticker="ETH" price="$2,500" />
+        </tbody>
+      </table>
     </div>
   )
 }
