7f486724e195d17b107ba46a5304c92be2a98a08
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-17 17:31:35 -0400

    getting prices from API
    using Axios

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -3,6 +3,7 @@ import CoinList from './components/CoinList/CoinList'
 import AccountBalance from './components/AccountBalance/AccountBalance'
 import Header from './components/Header/Header'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const Div = styled.div`
   text-align: center;
@@ -13,37 +14,64 @@ class App extends React.Component {
     balance: 10000,
     showBalance: true,
     coinData: [
-      {
-        name: 'Bitcoin',
-        ticker: 'BTC',
-        balance: 0.35,
-        price: 35721.51,
-      },
-      {
-        name: 'Ethereum',
-        ticker: 'ETH',
-        balance: 3.759,
-        price: 2513.05,
-      },
-      {
-        name: 'Tether',
-        ticker: 'USDT',
-        balance: 1000.0,
-        price: 1.0,
-      },
-      {
-        name: 'Ripple',
-        ticker: 'XRP',
-        balance: 120.0,
-        price: 0.43,
-      },
-      {
-        name: 'Bitcoin Cash',
-        ticker: 'BCH',
-        balance: 0.0,
-        price: 298.99,
-      },
+      // {
+      //   name: 'Bitcoin',
+      //   ticker: 'BTC',
+      //   balance: 0.35,
+      //   price: 35721.51,
+      // },
+      // {
+      //   name: 'Ethereum',
+      //   ticker: 'ETH',
+      //   balance: 3.759,
+      //   price: 2513.05,
+      // },
+      // {
+      //   name: 'Tether',
+      //   ticker: 'USDT',
+      //   balance: 1000.0,
+      //   price: 1.0,
+      // },
+      // {
+      //   name: 'Ripple',
+      //   ticker: 'XRP',
+      //   balance: 120.0,
+      //   price: 0.43,
+      // },
+      // {
+      //   name: 'Bitcoin Cash',
+      //   ticker: 'BCH',
+      //   balance: 0.0,
+      //   price: 298.99,
+      // },
     ],
+  }
+
+  componentDidMount = async () => {
+    const res = await axios.get('https://api.coinpaprika.com/v1/coins')
+
+    //this will give us the first 10 coins in string
+    const coinIds = res.data.slice(0, 10).map((coin) => coin.id)
+    const tickerUrl = 'https://api.coinpaprika.com/v1/tickers/'
+    // because it's a string, no need for object {key}, we can have just key
+    const promises = coinIds.map((id) => axios.get(tickerUrl + id))
+    // returns array of promises
+    const cointData = await Promise.all(promises)
+    // change coinData
+    const coinPriceData = cointData.map(function (response) {
+      const coin = response.data
+      return {
+        key: coin.id,
+        name: coin.name,
+        ticker: coin.symbol,
+        balance: 0,
+        price: parseFloat(Number(coin.quotes.USD.price).toFixed(4)),
+      }
+    })
+    // when taking the number toFixed(2), it will round to 2 decimal places but as a string
+    // to convert it into a number, use parseFloat(Number(12.32132154).toFixed(2))
+    // retrieve the prices
+    this.setState({ coinData: coinPriceData })
   }
 
   handleRefresh = (changedTickerValue) => {
